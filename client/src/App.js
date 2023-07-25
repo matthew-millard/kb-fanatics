28da7ef938ad48c3c29e3c5c84dd8c350443452c
matthew-millard <123032297+matthew-millard@users.noreply.github.com>
2023-07-25 14:55:41 -0400

    Feat: add  global layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,14 @@
+import styles from './App.module.css';
+import { Footer, Header, Main } from './components';
+
 function App() {
-  return null;
+  return (
+    <div className={styles.pageContainer}>
+      <Header className={styles.header} />
+      <Main className={styles.main} />
+      <Footer className={styles.footer} />
+    </div>
+  );
 }
 
 export default App;
