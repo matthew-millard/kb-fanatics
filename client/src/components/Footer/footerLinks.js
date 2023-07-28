c37df4a08a22f4b5e8c697d39c9236f06922aa8f
matthew-millard <123032297+matthew-millard@users.noreply.github.com>
2023-07-28 15:02:31 -0400

    Feat: add footer component

---
+++ client/src/components/Footer/footerLinks.js
@@ -0,0 +1,22 @@
+export const links = [
+  {
+    title: 'About Us',
+    url: '/about-us',
+  },
+  {
+    title: 'Contact Us',
+    url: '/contact-us',
+  },
+  {
+    title: 'FAQ',
+    url: '/faq',
+  },
+  {
+    title: 'Refund Policy',
+    url: '/refund-policy',
+  },
+  {
+    title: 'Terms of Service',
+    url: '/terms',
+  },
+];
