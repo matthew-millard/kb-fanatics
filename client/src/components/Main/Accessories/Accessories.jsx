2bbb55b8e67c64bb498952937efa9380caa6abd3
MarcoSaverino <marcosaverino@live.ca>
2023-07-31 21:22:05 -0400

    started the routes as discussed with group

---
+++ client/src/components/Main/Accessories/Accessories.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import styles from "./Accessories.module.css"
+function Accessories() {
+  return <h1 className={styles.h1}>Accessories</h1>;
+}
+
+export default Accessories;
---
+++ client/src/components/Main/Switches/Switches.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import styles from "./Switches.module.css"
+function Switches() {
+  return <h1 className={styles.h1}>Switches</h1>;
+}
+
+export default Switches;
