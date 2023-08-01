9e56c3d16567f269a640f3373b27c067eb5f6f15
rethissunstar <rethissunstar@gmail.com>
2023-08-01 18:48:16 -0400

    this is now have the models and seeds working need to work on orders

--- server/models/index.js
+++ server/models/index.js
@@ -5,4 +5,4 @@ import Keyboard from "./Keyboards.js";
 import Order from "./Order.js"
 //I left with the ./Order.js but it's telling me it should just be .order.js
 
-export default { SwitchModel, User, Keycap, Keyboard, Order };;
+export { SwitchModel, User, Keycap, Keyboard, Order };;
