f9e3256b559804944b307e67e68e8c9fcf7f83c1
matthew-millard <123032297+matthew-millard@users.noreply.github.com>
2023-08-15 14:36:52 -0400

    Heroku package.json config

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import express from "express";
 import { ApolloServer } from "apollo-server-express";
 import { typeDefs, resolvers } from "./schemas/index.js";
 import db from "./config/connection.js";
+import path from "path";
 
 const PORT = process.env.PORT || 3001;
 
@@ -16,6 +17,15 @@ const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, "client/build")));
+
+// The "catchall" handler: for any request that doesn't match any above,
+// send back the React index.html file.
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+});
 
 const startApolloServer = async () => {
   await server.start();
