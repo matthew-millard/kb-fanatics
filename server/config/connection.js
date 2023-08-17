44c157c13fd2528e0e1f34ae83f5b1ad886a56f3
matthew-millard <123032297+matthew-millard@users.noreply.github.com>
2023-08-16 21:35:59 -0400

    Deploy to Heroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,5 @@
 import mongoose from "mongoose";
 import "dotenv/config";
-console.log(process.env.MONGODB_URI);
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/kbfanatics-db", {
   useNewUrlParser: true,
