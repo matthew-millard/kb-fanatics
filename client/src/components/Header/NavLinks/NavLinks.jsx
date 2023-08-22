12d6b27896256b0868db035e551bb40725f72c93
matthew-millard <123032297+matthew-millard@users.noreply.github.com>
2023-08-22 16:18:30 -0400

    Feat: hightlight active navigation link

--- client/src/components/Header/NavLinks/NavLinks.jsx
+++ client/src/components/Header/NavLinks/NavLinks.jsx
@@ -1,15 +1,21 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import PropTypes from "prop-types";
 import navLinks from "./navLinks";
 import styles from "./NavLinks.module.css";
 
 function NavLinks({ mobile }) {
+  const location = useLocation();
   return (
     <ul className={mobile ? styles.navLinksMobile : styles.navLinks}>
       {navLinks.map((linkObj) => (
         <li key={linkObj.title}>
-          <Link to={linkObj.url}>{linkObj.title}</Link>
+          <Link
+            to={linkObj.url}
+            className={location.pathname === linkObj.url ? styles.activeLink : ""}
+          >
+            {linkObj.title}
+          </Link>
         </li>
       ))}
     </ul>
